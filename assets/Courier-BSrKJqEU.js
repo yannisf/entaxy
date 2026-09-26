3324d70d5c431b9de7aed03bfafd7171c615204b
yannisf <yannisf@users.noreply.github.com>
2026-09-26 07:35:46 +0000

    deploy: yannisf/taxy@1d1a8d21387cb30842bb957df2edfea06466d397

--- assets/Courier-BSrKJqEU.js
+++ assets/Courier-BSrKJqEU.js
@@ -1,4 +1,4 @@
-import{p as e}from"./index-LmT67E6p.js";var t=e(((e,t)=>{var n={vfs:{"data/Courier.afm":{data:`StartFontMetrics 4.1
+import{p as e}from"./index-DM1BO4ui.js";var t=e(((e,t)=>{var n={vfs:{"data/Courier.afm":{data:`StartFontMetrics 4.1
 Comment Copyright (c) 1989, 1990, 1991, 1992, 1993, 1997 Adobe Systems Incorporated.  All Rights Reserved.
 Comment Creation Date: Thu May  1 17:27:09 1997
 Comment UniqueID 43050
